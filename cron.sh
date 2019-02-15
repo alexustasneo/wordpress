2681960a403aaf93e2cca4ea9a339115429725fa
alexustasneo <47296216+alexustasneo@users.noreply.github.com>
2019-02-15 05:31:57 +0200

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker-compose -f /wordpress/wp.txt exec -e MYSQL_USER=exampleuser -e MYSQL_PASSWORD=examplepass db sh /wordpress/backup.sh
+sudo docker-compose -f /wordpress/wp.txt exec db mysqldump -u exampleuser -pexamplepass exampledb > /wordpress/mysql.dump
 sudo aws s3 cp /wordpress/mysql.dump s3://dev-1-backet
