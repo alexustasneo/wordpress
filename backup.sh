0052f5dea028cb3096d682c3a3f2c5f5cac5d67d
alexustasneo <47296216+alexustasneo@users.noreply.github.com>
2019-02-15 04:34:44 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD exampledb > /wordpress/mysql-$(date +%Y-%m-%d-%H-%M).dump
+mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD exampledb > /wordpress/mysql.dump
